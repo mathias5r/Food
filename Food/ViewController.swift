5907535f20261b29d641a979eb1011ae30cb8731
Mathias <mathiassilva4@gmail.com>
2025-03-13 22:15:46 -0300

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Mathias da Rosa on 13/03/25.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    // lazy is being used here to initialize mapView only once
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI();
+    }
+    
+    private func setupUI() {
+        view.addSubview(mapView)
+        
+        // add constraints
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+
