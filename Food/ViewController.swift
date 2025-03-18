4a08e2c799227627c115fa217c4e4d0a9484b694
Mathias <mathiassilva4@gmail.com>
2025-03-17 23:58:24 -0300

    feat: add search text field to the view

--- Food/ViewController.swift
+++ Food/ViewController.swift
@@ -18,6 +18,18 @@ class ViewController: UIViewController {
         return map
     }()
     
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 16
+        searchTextField.clipsToBounds = true // Subview are clippled by the bounds of view
+        searchTextField.placeholder = "Search for Foods"
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI();
@@ -26,11 +38,18 @@ class ViewController: UIViewController {
     private func setupUI() {
         view.addSubview(mapView)
         
-        // add constraints
         mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(searchTextField)
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
     }
 }
 
