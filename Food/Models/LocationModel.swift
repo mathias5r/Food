24afa19fcaff9abbdec48ea327108a319891463a
Mathias <mathiassilva4@gmail.com>
2025-05-30 09:56:35 -0300

    feat: create models for restaurant, location and address

---
+++ Food/Models/LocationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModel.swift
+//  Food
+//
+//  Created by Mathias da Rosa on 30/05/25.
+//
+
+import Foundation
+
+struct LocationModel {
+    let lat: Double
+    let long: Double
+}
